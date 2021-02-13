0f2fa4a9c378f502a9f56ad1463fbdfc29e53bbd
daniel08022 <74110809+daniel08022@users.noreply.github.com>
2021-02-13 12:23:55 -0500

    Inicio proyecto

---
+++ js/navMobile.js
@@ -0,0 +1,15 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+        menu = document.querySelector('#menu');
+        // animacion menu
+        iconoMenu.addEventListener('click', (e) => {
+
+            menu.classList.toggle('active')
+        // icono del menu
+        const rutaActual = e.target.getAttribute('src');
+
+        if (rutaActual == 'assets/burger.svg'){
+            e.target.setAttribute('src', 'assets/close.svg')
+        }else{
+            e.target.setAttribute('src', 'assets/burger.svg')
+        }
+        });
