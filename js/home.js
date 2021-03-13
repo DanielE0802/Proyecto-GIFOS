4556f2c8aebc46b69e6ab4510a4f9f4c5b639875
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-13 17:17:06 -0500

    update

--- js/home.js
+++ js/home.js
@@ -1,32 +1,55 @@
-let bt_fav = document.getElementById("btn_fav")
-let fav= document.getElementById("favoritos")
-let section_fav_btn = document.getElementById("section_fav_btn")
-let bt_mis_gifos = document.getElementById("btn_mis_gifos")
-let mis_gif= document.getElementById("mis-gifos")
-let section_mis_gifos_btn = document.getElementById("section_mis_gifos_btn")
+function call (id){
+    return document.getElementById(id)
+}
+
+let bt_fav = call("btn_fav")
+let fav= call("favoritos")
+let section_fav_btn = call("section_fav_btn")
+let bt_mis_gifos = call("btn_mis_gifos")
+let mis_gif= call("mis-gifos")
+let section_mis_gifos_btn = call("section_mis_gifos_btn")
+let btn_crear_gifos= call("btn_crear_gifos")
+let seccion_crear_gifos=call("crearGifos")
+let body_inicio=call("body_inicio")
 
 
+//Sección crear GIFO
+
+btn_crear_gifos.addEventListener('click' ,function(){
+    body_inicio.classList.toggle("display-none")
+    seccion_crear_gifos.classList.toggle("display-none")
+},false)
+
+
+//Sección favoritos
 
 bt_fav.addEventListener('click', function(e){
-    fav.classList.toggle('display-none');
-    section_fav_btn.classList.toggle('display-none');
-
-    
+    fav.classList.remove('display-none'); 
+    section_fav_btn.classList.remove('display-none');
     mis_gif.classList.add("display-none")
     section_mis_gifos_btn.classList.add('display-none');
 
+    if(body_inicio.classList.value == "display-none"){
+        seccion_crear_gifos.classList.add("display-none")
+        body_inicio.classList.remove("display-none")
+    }
+
 },false)
 
 
+//Sección mis GIFOS
 
-
-bt_mis_gifos.addEventListener('click', function(i){
-    mis_gif.classList.toggle('display-none');
-    section_mis_gifos_btn.classList.toggle('display-none');
-
+bt_mis_gifos.addEventListener('click', function(e){
+    mis_gif.classList.remove('display-none');
+    section_mis_gifos_btn.classList.remove('display-none');
     fav.classList.add("display-none")
     section_fav_btn.classList.add('display-none');
-},false)
 
+    if(body_inicio.classList.value == "display-none"){
+        seccion_crear_gifos.classList.add("display-none")
+        body_inicio.classList.remove("display-none")
+    }
+    
+},false)
 
 
