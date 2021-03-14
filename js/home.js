391bca43d6e9a6dd8a2bd03c2b16930a2541357c
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-14 11:25:57 -0500

    update

--- js/home.js
+++ js/home.js
@@ -2,35 +2,47 @@ function call (id){
     return document.getElementById(id)
 }
 
+//Botones
 let bt_fav = call("btn_fav")
-let fav= call("favoritos")
-let section_fav_btn = call("section_fav_btn")
-let bt_mis_gifos = call("btn_mis_gifos")
-let mis_gif= call("mis-gifos")
-let section_mis_gifos_btn = call("section_mis_gifos_btn")
 let btn_crear_gifos= call("btn_crear_gifos")
-let seccion_crear_gifos=call("crearGifos")
+let btn_mis_gifos = call("btn_mis_gifos")
+let btn_home = call("logo")
+
+
+//Secciones
+let section_fav= call("favoritos")
+let section_fav_btn = call("section_fav_btn")
+
+let section_mis_gifos= call("mis-gifos")
+let section_mis_gifos_btn = call("section_mis_gifos_btn")
+
 let body_inicio=call("body_inicio")
-let logoNav = call(logo)
+let section_home = call("home")
+let section_crear_gifos=call("crearGifos")
+let section_main=call("section_main")
+
 
 //Sección crear GIFO
 
 btn_crear_gifos.addEventListener('click' ,function(){
     body_inicio.classList.toggle("display-none")
-    seccion_crear_gifos.classList.toggle("display-none")
+    section_crear_gifos.classList.toggle("display-none")
 },false)
 
 
 //Sección favoritos
 
 bt_fav.addEventListener('click', function(e){
-    fav.classList.remove('display-none'); 
+    section_fav.classList.remove('display-none'); 
+    section_fav.classList.add('padding'); 
     section_fav_btn.classList.remove('display-none');
-    mis_gif.classList.add("display-none")
+
+    section_mis_gifos.classList.add("display-none")
     section_mis_gifos_btn.classList.add('display-none');
+    section_main.classList.add("display-none")
 
     if(body_inicio.classList.value == "display-none"){
-        seccion_crear_gifos.classList.add("display-none")
+        section_crear_gifos.classList.add("display-none")
         body_inicio.classList.remove("display-none")
     }
 
@@ -39,16 +51,35 @@ bt_fav.addEventListener('click', function(e){
 
 //Sección mis GIFOS
 
-bt_mis_gifos.addEventListener('click', function(e){
-    mis_gif.classList.remove('display-none');
+btn_mis_gifos.addEventListener('click', function(e){
+    section_mis_gifos.classList.remove('display-none');
+    section_mis_gifos.classList.add("padding")
     section_mis_gifos_btn.classList.remove('display-none');
-    fav.classList.add("display-none")
+
+    section_fav.classList.add("display-none")
     section_fav_btn.classList.add('display-none');
+    section_main.classList.add("display-none")
 
     if(body_inicio.classList.value == "display-none"){
-        seccion_crear_gifos.classList.add("display-none")
+        section_crear_gifos.classList.add("display-none")
         body_inicio.classList.remove("display-none")
     }
 
 },false)
 
+btn_home.addEventListener('click', function(){
+    section_main.classList.remove('display-none')
+
+    section_fav.classList.add('display-none')
+    section_fav_btn.classList.add('display-none');
+    
+    section_mis_gifos.classList.add("display-none")
+    section_mis_gifos_btn.classList.add('display-none');
+
+    if(body_inicio.classList.value == "display-none"){
+
+        section_crear_gifos.classList.add("display-none")
+        body_inicio.classList.remove("display-none")
+    
+    }
+})
