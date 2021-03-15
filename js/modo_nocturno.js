53c3a6fcdcb45514ae61815ccc928df1046e09e0
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-14 20:01:29 -0500

    update

--- js/modo_nocturno.js
+++ js/modo_nocturno.js
@@ -5,6 +5,8 @@ function call(id) {
 let modo_nocturno = call("modo_nocturno")
 let btn_modo_nocturno = call("btn_modo_nocturno")
 
+//Imagenes
+let ruta_crear_gifos= btn_crear_gifos.getAttribute('src')
 
 btn_modo_nocturno.addEventListener('click', function (e) {
     modo_nocturno.classList.toggle("modo_nocturno");
@@ -16,6 +18,14 @@ btn_modo_nocturno.addEventListener('click', function (e) {
         btn_modo_nocturno.innerHTML="MODO NOCTURNO";
     }
 
+    let ruta_crear_gifos= btn_crear_gifos.getAttribute('src')
+
     // cambiar imagenes}
+    if(ruta_crear_gifos == 'assets/button-crear-gifo.svg'){
+        btn_crear_gifos.setAttribute('src','assets/CTA-crar-gifo-modo-noc.svg')
+    }
+    else{
+        btn_crear_gifos.setAttribute('src', 'assets/button-crear-gifo.svg')
+    }
 
 }, false)
