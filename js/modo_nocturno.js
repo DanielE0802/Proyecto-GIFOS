0e5ff5257fb0912ba06e67193427968b447ff392
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-15 18:21:12 -0500

    Modo nocturno

--- js/modo_nocturno.js
+++ js/modo_nocturno.js
@@ -4,9 +4,7 @@ function call(id) {
 
 let modo_nocturno = call("modo_nocturno")
 let btn_modo_nocturno = call("btn_modo_nocturno")
-
-//Imagenes
-let ruta_crear_gifos= btn_crear_gifos.getAttribute('src')
+let ultimo_modo= "";
 
 btn_modo_nocturno.addEventListener('click', function (e) {
     modo_nocturno.classList.toggle("modo_nocturno");
@@ -14,8 +12,10 @@ btn_modo_nocturno.addEventListener('click', function (e) {
     // cambiar texto de modo nocturno a modo diurno
     if (btn_modo_nocturno.innerHTML == "MODO NOCTURNO"){
         btn_modo_nocturno.innerHTML="MODO DIURNO";
+        var ultimo_modo = localStorage.setItem('MODO_NOCTURNO', 'TRUE')
     }else{
         btn_modo_nocturno.innerHTML="MODO NOCTURNO";
+        var ultimo_modo = localStorage.setItem('MODO_NOCTURNO','FALSE')
     }
 
     let ruta_crear_gifos= btn_crear_gifos.getAttribute('src')
@@ -28,4 +28,22 @@ btn_modo_nocturno.addEventListener('click', function (e) {
         btn_crear_gifos.setAttribute('src', 'assets/button-crear-gifo.svg')
     }
 
+   
+
 }, false)
+
+var modo_local= localStorage.getItem('MODO_NOCTURNO')
+
+if(modo_local == "TRUE"){
+    modo_nocturno.classList.add("modo_nocturno");
+    if (btn_modo_nocturno.innerHTML == "MODO NOCTURNO"){
+        btn_modo_nocturno.innerHTML="MODO DIURNO";
+    }else{
+        btn_modo_nocturno.innerHTML="MODO NOCTURNO";
+    }
+    
+}
+
+
+
+console.log(ultimo_modo)
