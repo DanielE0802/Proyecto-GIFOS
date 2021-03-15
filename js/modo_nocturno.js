c8f8a54e0314d830d4de7dfef400a72f29c4d590
oscar2620 <74290676+oscar2620@users.noreply.github.com>
2021-03-14 19:40:29 -0500

    .

--- js/modo_nocturno.js
+++ js/modo_nocturno.js
@@ -1,15 +1,16 @@
-function call (id){
+function call(id) {
     return document.getElementById(id)
 }
 
 let modo_nocturno = call("modo_nocturno")
-let btn_modo_nocturno = call("btn_modo_nocturno") 
+let btn_modo_nocturno = call("btn_modo_nocturno")
 
-
-
-
-
-btn_modo_nocturno.addEventListener('click', function(e){
+btn_modo_nocturno.addEventListener('click', function (e) {
     modo_nocturno.classList.toggle("modo_nocturno");
-    btn_crear_gifos.setAttribute('src', 'assets/CTA-crear-gifo-hover-modo-noc.svg');
+    
+    if (btn_modo_nocturno.innerHTML == "MODO NOCTURNO"){
+        btn_modo_nocturno.innerHTML="MODO DIURNO";
+    }else{
+        btn_modo_nocturno.innerHTML="MODO NOCTURNO";
+    }
 }, false)
