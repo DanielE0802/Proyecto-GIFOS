bfaff47b5b9661d6c0249b556121336f00ecab32
daniel08022 <74110809+DanielE0802@users.noreply.github.com>
2021-03-22 21:04:33 -0500

    update fav

--- js/Api.js
+++ js/Api.js
@@ -1,4 +1,7 @@
 const containerImg = document.querySelectorAll("#busqueda > div.galery-gifs >div > img")
+const containerh1 = document.querySelectorAll("#busqueda > div.galery-gifs > div > div > div.texto_layout > h1")
+const containerh2 = document.querySelectorAll("#busqueda > div.galery-gifs > div > div > div.texto_layout > h2")
+const containerIconFav = document.querySelectorAll("#busqueda > div.galery-gifs > div > div > div.iconos_layout > img:nth-child(1)")
 const titleBusqueda = document.getElementById('title-busqueda')
 const sectionBusqueda = document.getElementById('busqueda')
 const reaction = document.getElementById('reactions')
@@ -6,6 +9,7 @@ const entertainment = document.getElementById('entretainment')
 const sports = document.getElementById('sports')
 const stickers = document.getElementById('stickers')
 const artists = document.getElementById('artists')
+const btn_img_fav = document.querySelectorAll("#busqueda > div.galery-gifs > div > div > div.iconos_layout > img:nth-child(1)")
 
 search.addEventListener('keyup', function () {
     let search = document.getElementById("search")
@@ -27,13 +31,18 @@ function api(busqueda) {
 
     let info = callApiSearch();
     info.then(response => {
-        for (let i = 0; i <= 25; i++) {
+        for (var i = 0; i <= 12; i++) {
             containerImg[i].setAttribute("src", response.data[i].images.fixed_height.url)
+            console.log(response.data[i])
+            containerh1[i].innerHTML = response.data[i].username
+            containerh2[i].innerHTML = response.data[i].title
         }
 
     }).catch(error => {
         console.error(error);
     })
+
+ 
 }
 
 
@@ -50,8 +59,8 @@ function trending() {
 
     let info = callApiTrending();
     info.then(response => {
-        for (let i = 0; i <= 6; i++) {
-            sectionTrendingImg[i].setAttribute("src", response.data[i].images.fixed_height.url)
+        for (let u = 0; u <= 6; u++) {
+            sectionTrendingImg[u].setAttribute("src", response.data[u].images.fixed_height.url)
         }
 
     }).catch(error => {
@@ -85,12 +94,42 @@ artists.addEventListener('click', function () {
     api("Artists")
 })
 
+//fav section
+let divContenido = `alt="gif">
+<div class="layout">
+    <div class="iconos_layout">
+        <img src="assets/icon-fav.svg" alt="">
+        <img src="assets/icon-max-normal.svg" alt="">
+        <img class="btn_descarga" src="assets/icon-download.svg" alt="">
+    </div>
+    <div class="texto_layout">
+        <h1>titulo</h1>
+        <h2>subtitulo</h2>
+    </div>
+</div>` 
 
-for (let i = 0; i < containerImg.length; i++) {
+let section_favoritos = document.getElementById('galery-fav')
 
-    containerImg[i].addEventListener('mouseenter', function () {
-        let div = document.createElement('span')
-        div.classList.add("mouseEncima")
-        containerImg[i].after(div)
+for (let i = 0; i <= 25; i++) {
+  
+    btn_img_fav[i].addEventListener('click', function () {
+        let crearFav = document.createElement('div');
+        section_favoritos.appendChild(crearFav)
+        console.log(crearFav)
+        let imgSrc = containerImg[i].getAttribute("src")
+        crearFav.innerHTML = `<img src="${imgSrc}" ${divContenido}`
+    
+        let rutaImgActual = `assets/icon-fav.svg` 
+        let rutaSeleccionFav = `assets/icon-fav-active.svg`
+    
+        if(containerIconFav[i].getAttribute('src')  == rutaImgActual){
+            containerIconFav[i].setAttribute('src', rutaSeleccionFav)
+        }
+        else{
+            console.log("no")
+        }
     })
+
 }
+
+
